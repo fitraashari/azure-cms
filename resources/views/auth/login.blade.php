68a589de5d7bf6797a43837af9447d4891510f51
Mohammad Fitra Ashari <fitra.drive@gmail.com>
2020-08-14 12:29:49 +0700

    typo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,9 +14,9 @@
                         @if (session('error'))
                         <div class="alert alert-danger">{{ session('error') }}</div>
                         @endif
-                        
+
                         <div class="form-group row">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">Username</label>
+                            <label for="username" class="col-md-4 col-form-label text-md-right">Username/Email</label>
 
                             <div class="col-md-6">
                                 <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
